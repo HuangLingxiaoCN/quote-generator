390eb87dd9c6158b39e974c6ca5c468d5adcb149
Jason Huang <jasonhlx1213@gmail.com>
2020-09-14 18:55:31 +0300

    added some code for error handling

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
+let errorCounter = 0;
 
-// Loading Spinner Shown
-function loading() {
+function showLoadingSpinner() {
+  // HTML element property "hidden" is a boolean value
+  // which is true if the element is hidden; otherwise the value is false. 
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Remove Loading Spinner
-function complete() {
+function removeLoadingSpinner() {
+  // if loader.hidden is false
   if (!loader.hidden) {
     quoteContainer.hidden = false;
     loader.hidden = true;
@@ -21,7 +23,7 @@ function complete() {
 
 // Get Quote From API
 async function getQuote() {
-  loading();
+  showLoadingSpinner();
   // We need to use a Proxy URL to make our API call in order to avoid a CORS error
   const proxyUrl = 'https://desolate-lake-64422.herokuapp.com/';
   const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
@@ -30,6 +32,8 @@ async function getQuote() {
     const data = await response.json();
     // Check if Author field is blank and replace it with 'Unknown'
     if (data.quoteAuthor === '') {
+      // innerText (similar to innerHTML and textContent), but it 
+      // overlooks spacing and inner element tags
       authorText.innerText = 'Unknown';
     } else {
       authorText.innerText = data.quoteAuthor;
@@ -41,10 +45,22 @@ async function getQuote() {
       quoteText.classList.remove('long-quote');
     }
     quoteText.innerText = data.quoteText;
-    // Stop Loading, Show Quote
-    complete();
+    removeLoadingSpinner();
   } catch (error) {
-    getQuote();
+    errorCounter++;
+    // Add fetch failure limits to 10
+    // When it reaches 10, display error message
+    // and reset error number.
+    if(errorCounter < 10){
+      console.log('oops',error);
+      getQuote();
+    }
+    else {
+      quoteText.innerText = 'Something goes wrong';
+      authorText.innerText = '';
+      removeLoadingSpinner();
+      errorCounter = 0;
+    }
   }
 }
 
@@ -53,6 +69,7 @@ function tweetQuote() {
   const quote = quoteText.innerText;
   const author = authorText.innerText;
   const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  // window.open() method opens a new browser tab or window
   window.open(twitterUrl, '_blank');
 }
 
